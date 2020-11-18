02da886116d7bdded7da73771b21ace4321f2536
ashaw101 <74671127+ashaw101@users.noreply.github.com>
2020-11-18 17:10:28 +0100

    Add files via upload

---
+++ code/lessons.js
@@ -0,0 +1,93 @@
+let lessons = [
+    {
+        "id": 1001,
+        "title": "Maths High level",
+        "description": "Location : Malta",
+        "price": 250,
+        "image": "images/maths.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 2,
+    },
+    {
+        "id": 1002,
+        "title": "Art High level",
+        "description": "Location : Malta",
+        "price": 240,
+        "image": "images/art.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 4,
+    },
+    {
+        "id": 1003,
+        "title": "Sports High level",
+        "description": "Location : Malta",
+        "price": 230,
+        "image": "images/sports.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 4,
+    },
+    {
+        "id": 1004,
+        "title": "English High level",
+        "description": "Location : Malta",
+        "price": 220,
+        "image": "images/english.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 4,
+    },
+    {
+        "id": 1005,
+        "title": "Science High level",
+        "description": "Location : Malta",
+        "price": 210,
+        "image": "images/science.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 5,
+    },
+    {
+        "id": 1006,
+        "title": "Maths High level",
+        "description": "Location : Malta",
+        "price": 160,
+        "image": "images/maths.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 2,
+    },
+    {
+        "id": 1007,
+        "title": "Art Low level",
+        "description": "Location : Malta",
+        "price": 110,
+        "image": "images/art.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 4,
+    },
+    {
+        "id": 1008,
+        "title": "Sports Low level",
+        "description": "Location : Malta",
+        "price": 150,
+        "image": "images/sports.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 4,
+    },
+    {
+        "id": 1009,
+        "title": "English Low level",
+        "description": "Location : Malta",
+        "price": 120,
+        "image": "images/english.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 4,
+    },
+    {
+        "id": 1010,
+        "title": "Science Low level",
+        "description": "Location : Malta",
+        "price": 130,
+        "image": "images/science.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 5,
+    },
+
+]
